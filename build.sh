25b86e4b94e086d5e85934bf8b31b9718d039706
Josh Maximoff <jmaximoff@numurus.com>
2020-09-29 09:03:06 -0400

    Build script now builds timestamp.proto too

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ if [ ! -d ./py-proto ]; then
   mkdir ./py-proto
 fi
 protoc --proto_path=. --python_out=./py-proto nepi_messaging-all.proto
+protoc --proto_path=. --python_out=./py-proto timestamp.proto
